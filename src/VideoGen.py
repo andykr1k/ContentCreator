3ae6dbb7bac35bd19e54bdf1b98273a1262d573e
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-04-02 19:45:50 -0700

    Adds png and text to video

--- src/VideoGen.py
+++ src/VideoGen.py
@@ -15,20 +15,35 @@ def Generate():
 
     split_en = posttext_english.split()
     # split_es = translation.split()
-        
-    clip = VideoFileClip("./media/video.mp4").resize( (1080,1920) )
-    title = ImageClip("./media/reddit.png").set_start(0).set_duration(3).set_pos(("center","center")).resize(height=50)
-    clip = CompositeVideoClip([clip, title]).resize( (1080,1920) )
-    audioclip = AudioFileClip("./media/english.mp3")
-    clip = clip.set_audio(audioclip)
-    txt_clip = TextClip(split_en[0], fontsize = 5, color = 'white').set_start(3).set_duration(3).set_pos(("center","center")).resize(height=50)
+    a = []
+    i = 0
+    d = 1
+
+    clip = VideoFileClip("./media/video2.mp4")
+    a.append(clip)
+
+    title = ImageClip("./media/reddit.png").set_start(i).set_duration(d).set_pos(("center","center")).resize(height=100)
+    a.append(title)
+    i = i + 1
+    d = d/2
+
+    # audioclip = AudioFileClip("./media/english.mp3")
+    # clip = clip.set_audio(audioclip)
+
+    for word in split_en:
+        txt_clip = TextClip(word, fontsize = 50, color = 'white').set_start(i).set_duration(d).set_pos(("center","center"))
+        a.append(txt_clip)
+        i = i + 1
+
     # im_width, im_height = txt_clip.size
     # color_clip = ColorClip(size=(int(im_width*1.1), int(im_height*1.4)), color=(0, 255, 255)).set_start(3).set_duration(3)
     # color_clip = color_clip.set_opacity(.6)
     # clip_to_overlay = CompositeVideoClip([color_clip, txt_clip]).resize( (1080,1920) )
     # clip_to_overlay = clip_to_overlay.set_position('center')
     # video = CompositeVideoClip([clip, clip_to_overlay]).resize( (1080,1920) )
-    video = CompositeVideoClip([clip, txt_clip]).resize( (1080,1920) )
-    video.write_videofile('./media/final_english.mp4',fps=60,codec='libx264')
+
+    clip = CompositeVideoClip(a)
+    
+    clip.write_videofile('./media/final_english.mp4',fps=60,codec='libx264')
 
 Generate()
