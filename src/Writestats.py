43ab9337c89637ebefeb2093e9190a12044bcde4
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-04-13 00:44:24 -0700

    Changed Uploader due to pricing issues

--- src/Writestats.py
+++ src/Writestats.py
@@ -1,16 +1,33 @@
-import time
-import asyncio
+import datetime
+import dotenv
+import os
+import requests
 
 def write():
+    dotenv.load_dotenv()
+    APY_API = os.environ.get("APY_API")
+
+    payload = {'platforms': ['tiktok']}
+    headers = {'Content-Type': 'application/json', 
+            'Authorization': 'Bearer API_KEY'}
+
+    r = requests.post('https://app.ayrshare.com/analytics/social', 
+        json=payload, 
+        headers=headers)
+        
+    print(r.json())
+    
     f = open("demofile2.txt", "a")
-    f.write("/n")
-    f.write("Date:")
-    f.write("/n")
+    f.write("\n")
+    f.write("Date: " + datetime.datetime.now())
+    f.write("\n")
     f.write("Post Title:")
-    f.write("/n")
+    f.write("\n")
+    f.write("Post Share ID: " + share_id)
+    f.write("\n")
     f.write("Current Follower Count:")
-    f.write("/n")
+    f.write("\n")
     f.write("Current Like Total:")
-    f.write("/n")
+    f.write("\n")
     f.write("Total # of Videos:")
     f.close()
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import VideoGen
-import PostContent
+import AutoUploader
 import Writestats
 
-VideoGen.Generate()
-Writestats.write()
-PostContent.upload_to_tiktok("./media/final_english.mp4", "test", ["test1", "test2", "test3"])
+# VideoGen.Generate()
+AutoUploader.upload_to_tiktok("./media/final_english.mp4")
+# Writestats.write()
